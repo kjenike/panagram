b9295a658ce47568f111167f498b12a8a37d3b10
skovaka <skovaka@gmail.com>
2025-03-16 11:09:09 -0400

    made cpp anchoring chunked

--- cpp/anchor.cpp
+++ cpp/anchor.cpp
@@ -10,11 +10,13 @@
 #include "htslib/bgzf.h"
 //#include "nc_utils.h"
 //
+//
+#define BIN_LEN 200000
 
 struct KMCdb {
     std::string root;
     std::vector<CKMCFile*> files;
-    int ngenomes, nbytes;
+    int ngenomes, nbytes, kmer_length;
 
     KMCdb(std::string root_, int ngenomes_) : root(root_), ngenomes(ngenomes_) {
         size_t ndbs = ceil(((float)ngenomes) / 32);
@@ -26,6 +28,7 @@ struct KMCdb {
             files[i] = new CKMCFile();
             files[i]->OpenForRA(ss.str());
             ss.str("");
+            kmer_length = files[i]->KmerLength();
         }
 
         nbytes = (int) ceil(((float)ngenomes ) / 8);
@@ -37,14 +40,14 @@ struct KMCdb {
 
         std::stringstream ss;
 
-        ss << root << "/anchor/" << name << "/bitmap1";
+        ss << root << "/anchor/" << name << "/bitmap.1";
         auto basename = ss.str();
         ss << ".gz";
         auto bitmap1 = bgzf_open(ss.str().c_str(), "w");
         bgzf_index_build_init(bitmap1);
         ss.str("");
 
-        ss << root << "/anchor/" << name << "/bitmap100";
+        ss << root << "/anchor/" << name << "/bitmap.100";
         auto basename100 = ss.str();
         ss << ".gz";
         auto bitmap100 = bgzf_open(ss.str().c_str(), "w");
@@ -80,7 +83,7 @@ struct KMCdb {
 
                         size_t s = name.find(' ');
                         chrs_out << name.substr(0,s) << "\t" << chr_num << "\t" << ksize << "\t0\n";
-		        chrs_out.flush();
+                        chrs_out.flush();
                         chr_num += 1;
                     }
                     name = line.substr(1);
@@ -93,7 +96,7 @@ struct KMCdb {
             auto ksize = write_bits(chr_num, seq, bitmap1, bitmap100, bitsum_out);
             size_t s = name.find(' ');
             chrs_out << name.substr(0,s) << "\t" << chr_num << "\t" << ksize << "\t0\n";
-	    chrs_out.flush();
+            chrs_out.flush();
         }
 
         bgzf_index_dump(bitmap1, basename.c_str(), ".gzi");
@@ -107,73 +110,87 @@ struct KMCdb {
 
     //void write_bits(std::string &seq, std::ofstream &bitmap1) {
     size_t write_bits(size_t chr_num, std::string &seq, BGZF *bitmap1, BGZF *bitmap100, std::ofstream &bitsum_out) {
-        size_t dbi = 0;
-        size_t n = nbytes;
-
-        std::vector<u8> bytes;
-        std::vector<int> popcnts;
-
-        size_t offs = 0;
-        for (size_t dbi = 0; dbi < files.size(); dbi++) {
-            if (nbytes <= 4) {
-                n = nbytes;
-            } else if (dbi == files.size()-1 and nbytes % 4 > 0) {
-                n = nbytes % 4;
-            } else {
-                n = 4;
-            }
-
-            std::vector<u32> ints;
-            files[dbi]->GetCountersForRead(seq, ints);
-
-            if (bytes.size() == 0) {
-                bytes.resize(ints.size()*nbytes);
-                popcnts.resize(ints.size());
-            }
-            
-            size_t i = offs;
-            //for (auto val : ints) {
-            for (size_t j = 0; j < ints.size(); j++) {
-                for (size_t sh = 0; sh < n; sh++) {
-                    bytes[i+sh] = (u8) (ints[j] >> (sh*8));
-                }
-                popcnts[j] += __builtin_popcount(ints[j]);
-                i += nbytes;
-            }
-
-            offs += n;
-        }
-
-        bgzf_write(bitmap1, (void*) bytes.data(), bytes.size() * sizeof(u8));
-
-        size_t size100 = ceil( ((float) (bytes.size() / nbytes)) / 100 );
-        for (size_t i = 0; i < bytes.size(); i += 100*nbytes) {
-            for (size_t b = 0; b < nbytes; b++) {
-                bytes[(i/100)+b] = bytes[i+b];
-            }
-        }
-        bytes.resize(size100*nbytes);
-        bgzf_write(bitmap100, (void*) bytes.data(), bytes.size() * sizeof(u8));
 
         size_t binlen = 200000;
-        if (popcnts.size() / binlen < 100) {
-            binlen = popcnts.size() / 100;
+        size_t nkmers = seq.size() - kmer_length + 1;
+        if (nkmers / binlen < 100) {
+            binlen = nkmers / 100;
         }
-        size_t size200k = ceil( ((float) popcnts.size()) / binlen );
-        for (size_t start = 0; start < popcnts.size(); start += binlen) {
+
+        i64 nchunks = (nkmers / binlen) + (nkmers % binlen != 0);
+        i64 chunk_start = 0;
+
+        for (size_t chunk = 0; chunk < nchunks; chunk++) {
+            auto chunk_end = std::min(chunk_start+binlen, nkmers);
+
+
+            auto seq_chunk = seq.substr(chunk_start, (chunk_end-chunk_start) + kmer_length - 1);
+            //std::cout << chunk_start << " " << chunk_end << " " << seq_chunk.size() << "\n";
+
+
+            size_t dbi = 0;
+            size_t n = nbytes;
+
+            std::vector<u8> bytes;
+            std::vector<int> popcnts;
+
+            size_t offs = 0;
+            for (size_t dbi = 0; dbi < files.size(); dbi++) {
+                if (nbytes <= 4) {
+                    n = nbytes;
+                } else if (dbi == files.size()-1 and nbytes % 4 > 0) {
+                    n = nbytes % 4;
+                } else {
+                    n = 4;
+                }
+
+                std::vector<u32> ints;
+                files[dbi]->GetCountersForRead(seq_chunk, ints);
+
+                if (bytes.size() == 0) {
+                    bytes.resize(ints.size()*nbytes);
+                    popcnts.resize(ints.size());
+                }
+                
+                size_t i = offs;
+                for (size_t j = 0; j < ints.size(); j++) {
+                    for (size_t sh = 0; sh < n; sh++) {
+                        bytes[i+sh] = (u8) (ints[j] >> (sh*8));
+                    }
+                    popcnts[j] += __builtin_popcount(ints[j]);
+                    i += nbytes;
+                }
+
+                offs += n;
+            }
+
+            bgzf_write(bitmap1, (void*) bytes.data(), bytes.size() * sizeof(u8));
+
+            size_t i100 = 0; //ceil( ((float) ((bytes.size() / nbytes)) / 100 ));
+            for (size_t i = ((100-(chunk_start % 100))%100)*nbytes; i < bytes.size(); i += 100*nbytes) {
+                for (size_t b = 0; b < nbytes; b++) {
+                    bytes[i100] = bytes[i+b];
+                    i100 += 1;
+                }
+            }
+            bytes.resize(i100);
+            bgzf_write(bitmap100, (void*) bytes.data(), bytes.size() * sizeof(u8));
+
             std::vector<size_t> bitsums;
             bitsums.resize(ngenomes+1);
-            for (size_t k = 0; k < binlen && start+k < popcnts.size(); k++) {
-                bitsums[popcnts[start+k]]++;
+            for (size_t k = 0; k < binlen && k < popcnts.size(); k++) {
+                bitsums[popcnts[k]]++;
             }
-            bitsum_out << chr_num << "\t" << start;
+            bitsum_out << chr_num << "\t" << chunk_start;
             for (auto c : bitsums) {
                 bitsum_out << "\t" << c;
             }
             bitsum_out << "\n";
-	    bitsum_out.flush();
+            bitsum_out.flush();
+            chunk_start += binlen;
         }
-        return popcnts.size();
+
+        return nkmers;
 
     }
 
