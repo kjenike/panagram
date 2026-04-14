c993d0c46e22e14e92d1ae9a6803ecb3094a9b3a
Nicole Brown <nbrown62@jhu.edu>
2026-04-13 20:30:21 -0400

    version bump

--- panagram/__about__.py
+++ panagram/__about__.py
@@ -1,5 +1,5 @@
 __title__ = "panagram"
-__version__ = "0.1.0"
+__version__ = "1.0.0"
 __summary__ = ("Pangenome viewer",)
 __author__ = "Katharine Jenike, Sam Kovaka, Nicole Brown"
 __uri__ = "https://github.com/kjenike/panagram"
